669d7e28a04227888aeeb530fcd16a18a6ba74f9
Kaslanas <victor24274@gmail.com>
2025-04-09 15:55:53 -0400

    Cours 2 UI

--- Assets/Scripts/MiniJeu.cs
+++ Assets/Scripts/MiniJeu.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MiniJeu : MonoBehaviour
 {
     public float pointageTemps;
 
+    public TextMeshProUGUI Text;
+
     void Start()
     {
         pointageTemps = 0;
+    }
+
+
+    private void Update()
+    {
+
+        Text.text = pointageTemps;
+
     }
 }
